5b7172827151b18ea5328513b23e75da79c8d09a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-25 06:04:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count.key?(genre)
-        genre_count[genre] = genre_count[genre] + 1
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
