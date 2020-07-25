b3683a5229c58f2fa12182c8f2d58d1a87631b39
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-25 06:03:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,6 +54,16 @@ class Song
     genre_count
   end
 
-
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count.key?(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[genre] = 1
+      end
+    end
+    artist_count
+  end
 
 end
